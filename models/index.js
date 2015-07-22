ebe1556e563264014a3b2042a13234997f884afd
KHAN Awais <ak07958@gmail.com>
2015-07-22 19:23:18 +0200

    Mise en commentaire de la chaine de connexion

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var process = {};
 process.env = {};
 // le tuto Azure voulait qu'on mette la chaine dans process.env.CUSTOMCONNSTR_MONGOLAB_URI
-process.env.CUSTOMCONNSTR_MONGOLAB_URI = 'mongodb://localhost:27017/stepCounter-dev-db';
+// process.env.CUSTOMCONNSTR_MONGOLAB_URI = 'mongodb://localhost:27017/stepCounter-dev-db';
 
 module.exports = function(app){
     app.mongoose = mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
