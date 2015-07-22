7af8702020c833bb575c57ac1a225520fafbf0ef
KHAN Awais <ak07958@gmail.com>
2015-07-23 00:08:01 +0200

    Ajout de debug

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ process.env = {};
 // le tuto Azure voulait qu'on mette la chaine dans process.env.CUSTOMCONNSTR_MONGOLAB_URI
 // process.env.CUSTOMCONNSTR_MONGOLAB_URI = 'mongodb://localhost:27017/stepCounter-dev-db';
 console.log(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+console.log("et process.env : ");
+console.log(process.env);
 
 module.exports = function(app){
     app.mongoose = mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
